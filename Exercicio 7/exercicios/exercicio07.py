d35601adec80952a72733a361ab872077df2c9ac
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2025-07-22 22:20:16 -0300

    Add o exercicio 07 de ER

---
+++ Exercicio 7/exercicios/exercicio07.py
@@ -0,0 +1,14 @@
+import re
+
+class Exercicio07:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def verificar(cls, texto):
+        p = re.compile('[@][A-Za-z]|[0-9_]')
+        if p.search(texto):
+            return print('String válida!')
+        else:
+            return print('String inválida!')
+
---
+++ Exercicio 7/main.py
@@ -0,0 +1,11 @@
+from exercicios.exercicio07 import Exercicio07 
+
+
+#Exercicio 07
+
+lista = ['@usuario', '@UFRA_2025','@nome123','@ usuario', '@', '@#nome123']
+
+for usuario_teste in lista:
+    Exercicio07.verificar(usuario_teste) 
+
+
